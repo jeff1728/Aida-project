24fb768a4347dfe7636a1a3a0520d5eb21515b74
Pam-ela-13 <pntapia1@espe.edu.ec>
2023-08-28 18:06:04 -0500

    Segundo commit

---
+++ src/aida/frame/PublicEntity.java
@@ -0,0 +1,21 @@
+package aida.frame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class PublicEntity {
+    /*
+    
+    Entidad Publica
+    Proporciona valores adeudados 
+    de los clientes de AIDA
+    
+    */
+    
+}
